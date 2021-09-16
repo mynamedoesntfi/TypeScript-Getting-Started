86a5dc6db6e38d3104a2c341e4194a11d914386e
Nandan Bhat <48284547+mynamedoesntfi@users.noreply.github.com>
2021-09-16 16:22:32 +0530

    Added annotated variables and functions

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,24 @@
 function startGame()    {
     // starting a new game
-    var messagesElement = document.getElementById("messages");
-    messagesElement!.innerText = "Welcome to MultiMath! Starting new game ...";
+    let playerName: string | undefined = getInputValue("playername");
+    logPlayer(playerName);
+
+    postScore(80, playerName);
+}
+
+function logPlayer(name: string = 'MultiMath Player'): void {
+    console.log(`New game starting for player: ${ name }`);
+}
+
+function getInputValue(elementID: string): string | undefined   {
+    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+    if(inputElement.value === '')   return undefined;
+    else return inputElement.value;
+}
+
+function postScore(score: number, playerName: string = 'MultiMath Player'): void {
+    const scoreElement: HTMLElement | null = document.getElementById("postedScores");
+    scoreElement!.innerText = `${score} - ${playerName}`;
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
